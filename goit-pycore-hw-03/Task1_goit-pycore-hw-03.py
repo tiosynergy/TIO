c8963d70035681cd2c27abc899106acd61ea598e
tiosynergy <tiosynergy@gmail.com>
2026-03-01 13:21:40 +0200

    1) Видалено глобальний код після if __name__ == "__main__""; 2) Виправленні помилки PEP8;

--- goit-pycore-hw-03/Task1_goit-pycore-hw-03.py
+++ goit-pycore-hw-03/Task1_goit-pycore-hw-03.py
@@ -3,48 +3,46 @@ from datetime import datetime
 
 
 def get_days_from_today(date) -> int: 
-    current_date=datetime.now().date()
+    current_date = datetime.now().date()
     target_date = datetime.strptime(date,"%Y-%m-%d").date()
     dlt=current_date - target_date
     return dlt.days
 
 # ----------- Перевірка введення року -------------
-if __name__ == "__main__":
-    while True:
-        try:
-            input_year=int(input("Введіть рік: "))
-            if type(input_year)==False:
-                continue
-            else:
-                break
-        except ValueError:
-            print("Помилка: Потрібно вводити тільки цифри!")
+while True:
+    try:
+        input_year = int(input("Введіть рік: "))
+        if type(input_year) == False:
+            continue
+        else:
+            break
+    except ValueError:
+        print("Помилка: Потрібно вводити тільки цифри!")
 
-    # ---------- Перевірка введення місяця ----------------
-    while True:
-        try:
-            input_month=int(input("Введіть міяць: "))
-            if type(input_month)==False or 0>= input_month >13:
-                continue
-            else:
-                break
-        except ValueError:
-            print("Помилка: Потрібно вводити тільки цифри!")
+# ---------- Перевірка введення місяця ----------------
+while True:
+    try:
+        input_month = int(input("Введіть міяць: "))
+        if type(input_month) == False or 0>= input_month >13:
+            continue
+        else:
+            break
+    except ValueError:
+        print("Помилка: Потрібно вводити тільки цифри!")
 
-    # ------------- Перевірка введення дня ---------------
-    while True:
-        try:
-            input_day=int(input("Введіть день: "))
-            if type(input_day)==False:
-                continue
-            else:
-                break
-        except ValueError:
-            print("Помилка: Потрібно вводити тільки цифри!")
+# ------------- Перевірка введення дня ---------------
+while True:
+    try:
+        input_day = int(input("Введіть день: "))
+        if type(input_day) == False:
+            continue
+        else:
+            break
+    except ValueError:
+        print("Помилка: Потрібно вводити тільки цифри!")
 
-
-inputed_date=f"{input_year}-{input_month}-{input_day}"
-print(type(inputed_date))
-current_date=datetime.today().date()
+#----- Формування вхідних данних-------
+inputed_date = f"{input_year}-{input_month}-{input_day}"
+current_date = datetime.today().date()
 
 print(f" кількість днів між\n заданою датою: {inputed_date}\n і поточною датою: {current_date} \n становить {get_days_from_today(inputed_date)}")
