f91a71f66bddd4af9163c453c70d038ea22b8e6e
tiosynergy <tiosynergy@gmail.com>
2026-03-07 09:41:10 +0200

    у першому і другому реалізовано if name == "main", у другому відкориговно PEP8

--- goit-pycore-hw-03/Task1_goit-pycore-hw-03.py
+++ goit-pycore-hw-03/Task1_goit-pycore-hw-03.py
@@ -2,47 +2,51 @@ import datetime
 from datetime import datetime
 
 
-def get_days_from_today(date) -> int: 
+def get_days_from_today(date) -> datetime: 
     current_date = datetime.now().date()
     target_date = datetime.strptime(date,"%Y-%m-%d").date()
-    dlt=current_date - target_date
-    return dlt.days
+    dlt = current_date - target_date
+    return dlt
 
+
+if __name__ == "__main__":
 # ----------- Перевірка введення року -------------
-while True:
-    try:
-        input_year = int(input("Введіть рік: "))
-        if type(input_year) == False:
-            continue
-        else:
-            break
-    except ValueError:
-        print("Помилка: Потрібно вводити тільки цифри!")
+    while True:
+        try:
+            input_year = int(input("Введіть рік: "))
+            if type(input_year) == False:
+                continue
+            else:
+                break
+        except ValueError:
+            print("Помилка: Потрібно вводити тільки цифри!")
 
-# ---------- Перевірка введення місяця ----------------
-while True:
-    try:
-        input_month = int(input("Введіть міяць: "))
-        if type(input_month) == False or 0>= input_month >13:
-            continue
-        else:
-            break
-    except ValueError:
-        print("Помилка: Потрібно вводити тільки цифри!")
+    # ---------- Перевірка введення місяця ----------------
+    while True:
+        try:
+            input_month = int(input("Введіть міяць: "))
+            if type(input_month) == False or 0 >= input_month >13:
+                continue
+            else:
+                break
+        except ValueError:
+            print("Помилка: Потрібно вводити тільки цифри!")
 
-# ------------- Перевірка введення дня ---------------
-while True:
-    try:
-        input_day = int(input("Введіть день: "))
-        if type(input_day) == False:
-            continue
-        else:
-            break
-    except ValueError:
-        print("Помилка: Потрібно вводити тільки цифри!")
+    # ------------- Перевірка введення дня ---------------
+    while True:
+        try:
+            input_day = int(input("Введіть день: "))
+            if type(input_day) == False:
+                continue
+            else:
+                break
+        except ValueError:
+            print("Помилка: Потрібно вводити тільки цифри!")
 
-#----- Формування вхідних данних-------
-inputed_date = f"{input_year}-{input_month}-{input_day}"
-current_date = datetime.today().date()
+    #----- Формування вхідних данних-------
+    inputed_date = f"{input_year}-{input_month}-{input_day}"
+    current_date = datetime.today().date()
 
-print(f" кількість днів між\n заданою датою: {inputed_date}\n і поточною датою: {current_date} \n становить {get_days_from_today(inputed_date)}")
+    #----Виклик основної функції----
+    delta = get_days_from_today(inputed_date).days
+    print(f" кількість днів між\n заданою датою: {inputed_date}\n і поточною датою: {current_date} \n становить {delta}")
--- goit-pycore-hw-03/Task2_goit-pycore-hw-03.py
+++ goit-pycore-hw-03/Task2_goit-pycore-hw-03.py
@@ -1,21 +1,24 @@
 import random
 
-# --------перевіряє обмеження та надає:
-#  список випадкових чисел або порпожній список--------------    
-def get_numbers_ticket(min, max, quantity) ->[]:
-    if min>=1 and min<max<1000 and min<=quantity<=max:
-        digit_random_list=random.sample(range(min,max),quantity)
-    else:
-        digit_random_list=[]
-    return digit_random_list
+
+def get_numbers_ticket(min, max, quantity) ->list:
+    """перевіряє обмеження та надає:
+       список випадкових чисел або порпожній список"""
+    if (
+        1 <= min
+        and min < max <= 1000
+        and min <= quantity <= max
+    ):
+        return random.sample(range(min, max + 1), quantity)
+    return []
 
 
 #-----------Введення та перевірка типів вхідних данних------
 if __name__ == "__main__":
     while True:
         try:
-            min_digit=int(input("Введи мінімальне число >=1:"))
-            if type(min_digit)==False:
+            min_digit = int(input("Введи мінімальне число >=1:"))
+            if type(min_digit) == False:
                 continue
             else:
                 break
@@ -24,8 +27,8 @@ if __name__ == "__main__":
 
     while True:
         try:
-            max_digit=int(input("Вееди максимальне число <=1000:"))
-            if type(max_digit)==False:
+            max_digit = int(input("Вееди максимальне число <=1000:"))
+            if type(max_digit) == False:
                 continue
             else:
                 break
@@ -33,8 +36,8 @@ if __name__ == "__main__":
             print("Помилка: Потрібно вводити тільки цифри!")
     while True:
         try:
-            qtt_digits=int(input(f"Введи необхідну кількість випадкових чисел в діапазоні до:{max_digit-min_digit}: "))
-            if type(qtt_digits)==False:
+            qtt_digits = int(input(f"Введи необхідну кількість випадкових чисел в діапазоні до:{max_digit-min_digit}: "))
+            if type(qtt_digits) == False:
                 continue
             else:
                 break
@@ -43,4 +46,5 @@ if __name__ == "__main__":
 
 
 # ------------ Виклик Функції---------------
-print(f"Набір унікальних випадкових чисел {get_numbers_ticket(min_digit,max_digit,qtt_digits)}")
+    result = get_numbers_ticket(min_digit, max_digit, qtt_digits)
+    print(f"Набір унікальних випадкових чисел {result}")
